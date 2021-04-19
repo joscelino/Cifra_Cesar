ae093ea5b760032676e20d59e6ce8548e5c2ffb6
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-04-19 20:12:20 -0300

    Docstrings

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
-from string import ascii_lowercase as alpha
-from operator import mod
 from functools import partial
-
+from operator import mod
+from string import ascii_lowercase as alpha
 
 t = len(alpha)
 
 
 def calc(letra, tipo="encrypt"):
+    """
+    Calculates expression to encrypt and decrypt.
+    :param letra: input string.
+    :param tipo: string type.
+    :return: expression.
+    """
     if tipo == "encrypt":
         exp = alpha.index(letra) + 3
     else:
@@ -15,6 +20,11 @@ def calc(letra, tipo="encrypt"):
 
 
 def encrypt(frase: str) -> str:
+    """
+    Encryption method.
+    :param frase: input string.
+    :return: encrypted string.
+    """
     return "".join(map(calc, frase))
 
 
@@ -22,4 +32,9 @@ dec = partial(calc, tipo="decrypt")
 
 
 def decrypt(frase: str) -> str:
+    """
+    Decrypt method.
+    :param frase: input string.
+    :return: decrypted string.
+    """
     return "".join(map(dec, frase))
